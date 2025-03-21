2ee88c3f0994340833bf6f5bd10862c70618a5f4
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2025-03-20 22:05:06 -0300

    Creating Page Structures
    This time we start creating the structure of the page where our Virtual Pet will live.

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+
+var name;
+var age;
+var hunger;
+var thirst;
+var happiness;
+var sleepiness;
+var hygiene;
+var mood;
+
+var stage; //"baby", "child", "adult"
+var is_alive;
+
+function Eat(){
+
+}
+
+function Drink(){
+
+}
+
+function Play(){
+
+}
+
+function Sleep(){
+
+}
+
+function Bath(){
+
+}
+
+function TimeCycle(){
+	
+}
